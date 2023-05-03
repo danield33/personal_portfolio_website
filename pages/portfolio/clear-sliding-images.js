b996eabab804e2b44078e7cbb1b237ef06d39838
danield33 <75221181+danield33@users.noreply.github.com>
2023-05-03 18:13:01 -0400

    add animation styles

--- pages/portfolio/clear-sliding-images.js
+++ pages/portfolio/clear-sliding-images.js
@@ -1,6 +1,22 @@
-function clearImages(sectionID){
+function clearImages(sectionID) {
 
     const section = $(`#${sectionID}`);
     console.log(section)
+
+    const $image = $('#image-1');
+
+    const imageHeight = $image.height();
+    const imageWidth = $image.width();
+
+    const height = ($(window).height() - imageHeight) / 2;
+    const width = ($(window).width() - imageWidth) / 2;
+    $image.addClass('position-fixed')
+    $image.removeClass("separator-box")
+        .addClass("flip-card")
+        .css({
+            top: height + 'px',
+            left: width + 'px'
+        })
+
 
 }
