941deb0f0b897c55c006771895d5e314ac91bea8
danield33 <75221181+danield33@users.noreply.github.com>
2023-05-04 22:46:41 -0400

    modal animation finished

--- pages/portfolio/clear-sliding-images.js
+++ pages/portfolio/clear-sliding-images.js
@@ -2,12 +2,28 @@ function showModal(sectionID) {
 
     const section = $(`#${sectionID}`);
 
-    const element = document.getElementById("parent-test-box");
+    const element = $(".layered-image");
     const box = $(`#test-box`);
 
-    $(element).addClass("freeze");
+    $(element[0]).addClass("freeze");
     box.addClass('expand');
-    const rect = box[0].getBoundingClientRect();
+
+
+    const [distanceX, distanceY] = getTranslationToCenterOfScreen(box[0]);
+    const scale = getScaleFactor(section, box);
+
+    box.css({
+        'transform': `matrix(3.13, 0.736, -3.86, 4.97, 0, 0) translate(${distanceX}px, ${distanceY}px) rotateY(180deg)  scale(${scale})`,
+    }).one('transitionend webkitTransitionEnd oTransitionEnd', () => {
+        console.log(23)
+    })
+    $(element[0]).css({
+        'z-index': 2 //moves it above text content
+    })
+}
+
+function getTranslationToCenterOfScreen(smallerElement) {
+    const rect = smallerElement.getBoundingClientRect();
 
 // Calculate the distance to the center of the screen
     const centerX = window.innerWidth / 2;
@@ -15,9 +31,12 @@ function showModal(sectionID) {
     //calculate distance to move
     const distanceX = centerX - rect.left - rect.width / 2;
     const distanceY = centerY - rect.top - rect.height / 2;
+    return [distanceX, distanceY];
+}
 
-    const parent = section[0];
-    const child = box[0];
+function getScaleFactor(biggerElement, smallerElement) {
+    const parent = biggerElement[0];
+    const child = smallerElement[0];
 
 // get the width and height of the parent and child elements
     const parentWidth = parent.getBoundingClientRect().width;
@@ -28,15 +47,5 @@ function showModal(sectionID) {
 // calculate the scale factor for the child element
     const scaleX = parentWidth / childWidth;
     const scaleY = parentHeight / childHeight;
-    const scale = Math.max(scaleX, scaleY);
-
-
-    box.css({
-        'transform': `matrix(3.13, 0.736, -3.86, 4.97, 0, 0) translate(${distanceX}px, ${distanceY}px) rotateY(180deg)  scale(${scale})`,
-    }).on('animationend', () => {
-        console.log(23)
-    })
-    $(element).css({
-        'z-index': 2 //moves it above text content
-    })
+    return Math.max(scaleX, scaleY);
 }
